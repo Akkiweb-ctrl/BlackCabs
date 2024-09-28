a9cae317f347ebbd64934d9161b62514015bb69f
Akkiweb-ctrl <rananawareakash001@gmail.com>
2024-09-28 20:05:05 +0530

    validations and spinners added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from "./components/Header/Header";
